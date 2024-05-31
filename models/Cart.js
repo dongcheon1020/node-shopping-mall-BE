bf3b94a6864b7f2296d534a56e277fc5b24cddca
dongcheon1020 <dongcheon1020@gmail.com>
2024-06-01 02:15:13 +0900

    feat:

---
+++ models/Cart.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+const cartSchema = Schema(
+  {
+    userId: { type: mongoose.ObjectId, ref: User },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product },
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+cartSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.updateAt;
+  return obj;
+};
+
+const Cart = mongoose.model("Cart", userSchema);
+module.exports.Cart;
